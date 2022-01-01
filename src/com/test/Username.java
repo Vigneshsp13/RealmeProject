182a66c70f8f59df81874b1ee51a67b4c23455f5
Vigneshsp13 <vigneshsp13@gmail.com>
2022-01-01 20:33:55 +0530

    Folder Structure

---
+++ src/com/test/Username.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Username {
+	
+	public static void username() {
+		System.out.println("User name");
+
+	}
+	
+	public static void main(String[] args) {
+		username();
+	}
+
+}
